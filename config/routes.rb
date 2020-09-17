77184595da41d18975377118a98b2237c762349f
msy7822-ux <msy780022@gmail.com>
2020-09-17 13:08:38 +0000

    65% complete profile pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # userに関するページのルーティング
+  get '/signup', to: 'users#new'
+  
+  # 紹介ページへのルーティング（静的ページ）
+  get '/home', to: 'profile_pages#home'
+  get '/about', to: 'profile_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'profile_pages#home'
 end
