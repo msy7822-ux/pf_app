a9a8a012dd35f6cc51f9847f474e66c9daa5abaa
msy7822-ux <msy780022@gmail.com>
2020-09-17 17:40:03 +0000

    add secure password and users model

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+    # before_saveというコールバック変数は、オブジェクトが保存される前の段階で実行される
+    before_save { self.email = self.email.downcase }
+    
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :univ_name, presence: true
+    
+    # ※emailのフォーマットは正規表現で作る
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/
+    validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+    # uniqueness { case_sensitive: false }とすることで、大文字小文字を無視した一意性の保持が可能
+    # ※SNSリンクに関してはなくても大丈夫(userモデルのSNSリンクは個人のSNSを想定、postモデルのSNSリンクはサークルの公式SNSを想定)
+    
+    # セキュアなパスワードを生成する
+    has_secure_password
+    # パスワードカラムに関するバリデーション
+    validates :passowrd, length: { minimum: 6 }
+end
