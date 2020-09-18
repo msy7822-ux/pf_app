29605db15f9fd8c198fceacf4a0db5a1a9938564
msy7822-ux <msy780022@gmail.com>
2020-09-18 07:56:05 +0000

    complete login form template

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
   # userに関するページのルーティング
   get '/signup', to: 'users#new'
   resources :users
