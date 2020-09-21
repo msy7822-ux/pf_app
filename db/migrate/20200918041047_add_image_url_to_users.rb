76161dd80d2599203211ce62bc033cca0d7fd42a
msy7822-ux <msy780022@gmail.com>
2020-09-21 01:38:30 +0000

    マイグレーションファイルの変更

--- db/migrate/20200918041047_add_image_url_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddImageUrlToUsers < ActiveRecord::Migration[5.2]
-  def change
-    # add_column :users, :image_url, :string
-    add_column :users, :image_url, 'bytea USING CAST(image_url AS bytea)'
-  end
-end
